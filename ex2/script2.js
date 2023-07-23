f2d07d2b42eba23bdd56e7fae632a9830c41400e
jonteste <biolocurso@gmail.com>
2023-07-23 19:25:32 -0300

    Correcoes

---
+++ ex2/script2.js
@@ -0,0 +1,10 @@
+// 2. Faça um programa que mostre a contagem de 10 a 1.
+let i = 10;
+function countDown() {
+    if (i >= 1) {
+        document.getElementById('contagem').textContent += i + ' ';
+        i--;
+        setTimeout(countDown, 1000); 
+    } 
+}
+countDown();
