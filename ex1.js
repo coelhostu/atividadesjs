30a13cb9d3928b557fa6bffd7c2c82dc90a247bc
jonteste <biolocurso@gmail.com>
2023-07-23 15:56:53 -0300

    Ex 1 e 2

--- ex1.js
+++ ex1.js
@@ -1,1 +1,7 @@
 // 1- Faça um programa que mostre a contagem de 1 a 10.
+
+console.log("-----Crecente------")
+for (let num1 = 1; num1 <=10; num1++) {
+    console.log(num1)
+
+} 
---
+++ ex2.js
@@ -0,0 +1,4 @@
+console.log("-----Decrecente------")
+for (let num2 = 10; num2 >=0; num2--) {
+    console.log(num2)
+}
